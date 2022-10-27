7c80cb9eba066bfd29a2ac0b4120c294985d9d0c
Patrick Decat <patrick.decat@fr.clara.net>
2022-10-27 14:50:49 +0200

    Avoid raising `TypeError: 'NoneType' object is not iterable` when resource listing returns None without error

--- res/glob.py
+++ res/glob.py
@@ -135,10 +135,11 @@ def get_inventory(ownerId,
                             inv_list = client.__getattribute__(function_name)(**additional_parameters).get(key_get)
                             utils.display(ownerId, region_name, aws_service, function_name)
 
-                            for inventory_object in inv_list:
+                            if inv_list:
+                                for inventory_object in inv_list:
 
-                                detailed_inv = get_inventory_detail(client, region_name, inventory_object, detail_function, join_key, detail_join_key, detail_get_key, pagination_detail, detail_additional_parameters)
-                                inventory.append(json.loads(utils.json_datetime_converter(detailed_inv)))
+                                    detailed_inv = get_inventory_detail(client, region_name, inventory_object, detail_function, join_key, detail_join_key, detail_get_key, pagination_detail, detail_additional_parameters)
+                                    inventory.append(json.loads(utils.json_datetime_converter(detailed_inv)))
 
                     except (botocore.exceptions.EndpointConnectionError, botocore.exceptions.ClientError) as e:
 
@@ -193,10 +194,10 @@ def get_inventory(ownerId,
                         detail = detail.get('DistributionList')
                     
                     # Anything in the detail item?
-
-                    for inventory_object in detail.get(key_get):
-                        detailed_inv = get_inventory_detail(client, aws_region, inventory_object, detail_function, join_key, detail_join_key, detail_get_key, pagination_detail, detail_additional_parameters)
-                        inventory.append(json.loads(utils.json_datetime_converter(detailed_inv)))
+                    if detail.get(key_get):
+                        for inventory_object in detail.get(key_get):
+                            detailed_inv = get_inventory_detail(client, aws_region, inventory_object, detail_function, join_key, detail_join_key, detail_get_key, pagination_detail, detail_additional_parameters)
+                            inventory.append(json.loads(utils.json_datetime_converter(detailed_inv)))
 
             else:
 
